709acef0879a688581efbeef958f2395222dc323
arc-source-coder <241987201+arc-source-coder@users.noreply.github.com>
2025-12-22 09:41:43 +0100

    tests: migrate matter.js
    - rename to matter.test.ts
    - rename previous matter.test.ts to matter-test.test.ts

--- tests/matter.test.ts
+++ tests/matter.test.ts
@@ -1,11 +1,107 @@
 import { describe, it, expect } from "bun:test";
 import matter from "../src/index";
+import { utils } from "../lib/utils";
 
-describe(".test", () => {
-  it("should return `true` if the string has front-matter:", () => {
-    expect(matter.test("---\nabc: xyz\n---")).toBeTruthy();
-    expect(matter.test("---\nabc: xyz\n---", { delims: "~~~" })).toBeFalsy();
-    expect(matter.test("~~~\nabc: xyz\n~~~", { delims: "~~~" })).toBeTruthy();
-    expect(matter.test("\nabc: xyz\n---")).toBeFalsy();
+describe("gray-matter", () => {
+  it("should extract YAML front matter", () => {
+    const actual = matter("---\nabc: xyz\n---");
+    expect(actual.hasOwnProperty("data")).toBeTruthy();
+    expect(actual.hasOwnProperty("content")).toBeTruthy();
+    expect(actual.hasOwnProperty("orig")).toBeTruthy();
+    expect(actual.data.abc).toEqual("xyz");
+  });
+
+  it('should cache original string as a buffer on the "orig property"', () => {
+    const fixture = "---\nabc: xyz\n---";
+    const actual = matter(fixture);
+    expect(utils.isBuffer(actual.orig)).toBeTruthy();
+    expect(actual.orig.toString()).toBe(fixture);
+  });
+
+  it("extra characters should throw parsing errors", () => {
+    expect(() => matter("---whatever\nabc: xyz\n---")).toThrow();
+  });
+
+  it("boolean yaml types should still return the empty object", () => {
+    const actual = matter("--- true\n---");
+    expect(actual.data).toEqual({});
+  });
+
+  it("string yaml types should still return the empty object", () => {
+    const actual = matter("--- true\n---");
+    expect(actual.data).toEqual({});
+  });
+
+  it("number yaml types should still return the empty object", () => {
+    const actual = matter("--- 42\n---");
+    expect(actual.data).toEqual({});
+  });
+
+  it("should throw an error when a string is not passed:", () => {
+    expect(() => matter()).toThrow();
+  });
+
+  it("should return an object when the string is 0 length:", () => {
+    expect(utils.isObject(matter(""))).toBeTruthy();
+  });
+
+  it("should extract YAML front matter and content", () => {
+    const fixture =
+      '---\nabc: xyz\nversion: 2\n---\n\n<span class="alert alert-info">This is an alert</span>\n';
+    const actual = matter(fixture);
+    expect(actual.data).toEqual({ abc: "xyz", version: 2 });
+    expect(actual.content).toBe(
+      '\n<span class="alert alert-info">This is an alert</span>\n',
+    );
+    expect(actual.orig.toString()).toBe(fixture);
+  });
+
+  it("should use a custom delimiter as a string.", () => {
+    const fixture =
+      '~~~\nabc: xyz\nversion: 2\n~~~\n\n<span class="alert alert-info">This is an alert</span>\n';
+    const actual = matter(fixture, { delims: "~~~" });
+    expect(actual.data).toEqual({ abc: "xyz", version: 2 });
+    expect(actual.content).toBe(
+      '\n<span class="alert alert-info">This is an alert</span>\n',
+    );
+    expect(actual.orig.toString()).toBe(fixture);
+  });
+
+  it("should use custom delimiters as an array.", () => {
+    const fixture =
+      '~~~\nabc: xyz\nversion: 2\n~~~\n\n<span class="alert alert-info">This is an alert</span>\n';
+    const actual = matter(fixture, { delims: ["~~~"] });
+    expect(actual.data).toEqual({ abc: "xyz", version: 2 });
+    expect(actual.content).toBe(
+      '\n<span class="alert alert-info">This is an alert</span>\n',
+    );
+    expect(actual.orig.toString()).toBe(fixture);
+  });
+
+  it("should correctly identify delimiters and ignore strings that look like delimiters.", () => {
+    const fixture =
+      '---\nname: "troublesome --- value"\n---\nhere is some content\n';
+    const actual = matter(fixture);
+    expect(actual.data).toEqual({ name: "troublesome --- value" });
+    expect(actual.content).toBe("here is some content\n");
+    expect(String(actual.orig)).toBe(
+      '---\nname: "troublesome --- value"\n---\nhere is some content\n',
+    );
+  });
+
+  it("should correctly parse a string that only has an opening delimiter", () => {
+    const fixture = '---\nname: "troublesome --- value"\n';
+    const actual = matter(fixture);
+    expect(actual.data).toEqual({ name: "troublesome --- value" });
+    expect(actual.content).toBe("");
+    expect(String(actual.orig)).toBe('---\nname: "troublesome --- value"\n');
+  });
+
+  it("should not try to parse a string has content that looks like front-matter.", () => {
+    const fixture = "-----------name--------------value\nfoo";
+    const actual = matter(fixture);
+    expect(actual.data).toEqual({});
+    expect(actual.content).toBe("-----------name--------------value\nfoo");
+    expect(String(actual.orig)).toBe("-----------name--------------value\nfoo");
   });
 });
