f5aaf0cb96e2263ae725cd43f8f98cde3892e8e7
arc-source-coder <241987201+arc-source-coder@users.noreply.github.com>
2025-12-22 12:07:03 +0100

    tests: migrate stringify.js

---
+++ tests/stringify.test.ts
@@ -0,0 +1,66 @@
+import { describe, it, expect } from "bun:test";
+import matter from "../src/index";
+
+describe(".stringify", () => {
+  it("should stringify front-matter from a file object", () => {
+    const file = { content: "Name: {{name}}\n", data: { name: "gray-matter" } };
+    expect(matter.stringify(file)).toBe(
+      ["---", "name: gray-matter", "---", "Name: {{name}}\n"].join("\n"),
+    );
+  });
+
+  it("should stringify from a string", () => {
+    expect(matter.stringify("Name: {{name}}\n")).toBe("Name: {{name}}\n");
+  });
+
+  it("should use custom delimiters to stringify", () => {
+    const data = { name: "gray-matter" };
+    const actual = matter.stringify("Name: {{name}}", data, { delims: "~~~" });
+    expect(actual).toBe(
+      ["~~~", "name: gray-matter", "~~~", "Name: {{name}}\n"].join("\n"),
+    );
+  });
+
+  it("should stringify a file object", () => {
+    const file = { content: "Name: {{name}}", data: { name: "gray-matter" } };
+    const actual = matter.stringify(file);
+    expect(actual).toBe(
+      ["---", "name: gray-matter", "---", "Name: {{name}}\n"].join("\n"),
+    );
+  });
+
+  it("should stringify an excerpt", () => {
+    const file = {
+      content: "Name: {{name}}",
+      data: { name: "gray-matter" },
+      excerpt: "This is an excerpt.",
+    };
+    expect(matter.stringify(file)).toBe(
+      [
+        "---",
+        "name: gray-matter",
+        "---",
+        "This is an excerpt.",
+        "---",
+        "Name: {{name}}\n",
+      ].join("\n"),
+    );
+  });
+
+  it("should not add an excerpt if it already exists", () => {
+    const file = {
+      content: "Name: {{name}}\n\nThis is an excerpt.",
+      data: { name: "gray-matter" },
+      excerpt: "This is an excerpt.",
+    };
+
+    expect(matter.stringify(file)).toBe(
+      [
+        "---",
+        "name: gray-matter",
+        "---",
+        "Name: {{name}}\n\nThis is an excerpt.\n",
+      ].join("\n"),
+    );
+  });
+});
