f4ee34364f119a854ed3cc5f1780d3380791d03e
arc-source-coder <241987201+arc-source-coder@users.noreply.github.com>
2025-12-22 11:43:11 +0100

    tests: migrate parse-custom.js
    TODO: fix YAML parser

---
+++ tests/parse-custom.test.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "bun:test";
+import matter from "../src/index";
+
+describe("custom parser:", function () {
+  it("should allow a custom parser to be registered:", () => {
+    const actual = matter.read("./test/fixtures/lang-yaml.md", {
+      parser: function customParser(str: string, opts) {
+        try {
+          // TODO: Add opts
+          return Bun.YAML.parse(str);
+        } catch (err) {
+          throw new SyntaxError(err);
+        }
+      },
+    });
+
+    expect(actual.data.title).toBe("YAML");
+    expect(actual.hasOwnProperty("data")).toBeTruthy();
+    expect(actual.hasOwnProperty("content")).toBeTruthy();
+    expect(actual.hasOwnProperty("orig")).toBeTruthy();
+  });
+});
